19cd05670ba9ca3017af3c643a02616aa3901e44
Vineeta Srivastava <vsrivastava@google.com>
2016-05-13 17:54:50 -0700

    marlin/sailfish: add sepolicy for verizon obdm tool
    Bug: 28765079
    Change-Id: I62df586b571b29a8f43216919c5c908980ce5e73

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -101,9 +101,7 @@ BOARD_ROOT_EXTRA_FOLDERS := bt_firmware firmware firmware/radio persist
 BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp
 
 BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy
-ifneq ($(filter marlin marlinf, $(TARGET_PRODUCT)),)
 BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy/verizon
-endif
 BOARD_SECCOMP_POLICY += device/google/marlin/seccomp
 
 BOARD_EGL_CFG := device/google/marlin/egl.cfg
